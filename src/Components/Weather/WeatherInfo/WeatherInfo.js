27876b0c4db84b825161d0a8ec4ca9d778b12784
Adam-harte <adamharte11@hotmail.co.uk>
2018-11-09 17:23:32 +0000

    WeatherInfo css classes applied to the WeatherInfo component

--- src/Components/Weather/WeatherInfo/WeatherInfo.js
+++ src/Components/Weather/WeatherInfo/WeatherInfo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import './WeatherInfo.css';
+
 const WeatherInfo = (props) => {
    return (
-      <div>
-         <h1>{props.description}</h1>
-         <p>{props.temp}</p>
-         <p>{props.wind}</p>
-         <p>{props.humidity}</p>
+      <div className="WeatherInfo">
+         <h1 className="description">{props.description}</h1>
+         <p className="temp">{props.temp}</p>
+         <p className="wind">{props.wind}</p>
+         <p className="humidity">{props.humidity}</p>
       </div>
    );
 };
