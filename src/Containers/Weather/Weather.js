f2138524462267410926f61fd2e3b8fc4bccc4ef
Adam-harte <adamharte11@hotmail.co.uk>
2018-11-15 17:57:18 +0000

    Input and Button components added to implement search functionality with handler methods implemented as well

--- src/Containers/Weather/Weather.js
+++ src/Containers/Weather/Weather.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 
 import WeatherDisplay from '../../Components/Weather/WeatherDisplay/WeatherDisplay';
+import Input from '../../Components/UI/Input/Input';
+import Button from '../../Components/UI/Button/Button';
 import Spinner from '../../Components/UI/Spinner/Spinner';
 
 class Weather extends Component {
    state = {
       data: null,
-      loading: true
+      loading: true,
+      location: ""
    }
 
    componentDidMount() {
       axios.get('?q=London&APPID=34d867e6bd0decb3b401e02be6a10993')
+         .then(res => {
+            console.log(res);
+            this.setState({ data: res.data, loading: false });
+         })
+         .catch(err => {
+            console.log(err);
+         });
+   }
+
+   locationChangeHanlder = (event) => {
+      this.setState({location: event.target.value});
+   }
+
+   searchWeatherHandler = () => {
+      axios.get('?q=' + this.state.location + '&APPID=34d867e6bd0decb3b401e02be6a10993')
          .then(res => {
             console.log(res);
             this.setState({ data: res.data, loading: false });
@@ -29,6 +47,8 @@ class Weather extends Component {
       }
       return (
          <div>
+            <Input inpType="text" inpPlaceholder="Location" inpValue={this.state.location} changed={this.locationChangeHanlder} />
+            <Button btnStyle="Success" clicked={this.searchWeatherHandler}>Search</Button>
             {content}
          </div>
       );
